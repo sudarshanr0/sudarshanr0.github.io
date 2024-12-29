a60a7a8f069248e5236cdf441c46a9699518c3c3
sudarshanr0 <drshirsu@gmail.com>
2024-12-29 16:57:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,22 @@ contactForm.addEventListener('submit', function (e) {
     });
 });
 
+// Get the button
+const backToTopButton = document.getElementById('back-to-top');
+
+// Show the button when the user scrolls down
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.classList.add('show-btn');
+    } else {
+        backToTopButton.classList.remove('show-btn');
+    }
+};
+
+// Scroll to the top when the button is clicked
+backToTopButton.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling
+    });
+});
